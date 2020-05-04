c76f37dddd04cab406fa5dacf47d3d750e5b1e26
YusofGotboudine <yusof95@hotmail.com>
2020-05-05 04:25:36 +0800

    help 2

---
+++ seed.sql
@@ -0,0 +1,20 @@
+INSERT INTO users
+    (name, password, email, description)
+VALUES
+    ('Cedric
+Llama', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '123', 'How you doin');
+
+INSERT INTO users
+    (name, password, email, description)
+VALUES
+    ('Kendric Lamar', '8d23cf6c86e834a7aa6eded54c26ce2bb2e74903538c61bdd5d2197997ab2f72', '321', 'Waddup');
+
+INSERT INTO users
+    (name, password, email, description)
+VALUES
+    ('Derrick Obama', '35a9e381b1a27567549b5f8a6f783c167ebf809f1c4d6a9e367240484d8ce281', '789', 'Hola');
+
+INSERT INTO users
+    (name, password, email, description)
+VALUES
+    ('Barry Osama', '55c8079ac96c6a4f6a94e3460c79e4006d62374cce6e9fc8b281938a3abc7627', '987', 'Hello');
